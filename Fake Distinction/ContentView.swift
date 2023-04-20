dc18e34e1aa4b1a2d07268c39d583619f56abbe9
Amane Kushima <popirubi0307@gmail.com>
2023-04-20 10:00:16 +0900

    no message

--- Fake Distinction/ContentView.swift
+++ Fake Distinction/ContentView.swift
@@ -44,11 +44,14 @@ struct ContentView: View {
             }
         } else {
             var cardColor = colorScheme == .dark ? K.Colors.cardDark : K.Colors.cardLight
+            
+            Spacer()
+            
             VStack {
-                ZStack {
+//                ZStack {
                     Group{
                         // イディオムカード
-                        VStack() {
+                        VStack(alignment: .center, spacing: 4) {
                             Text(idiom).font(.system(size: K.Size.idiomTextSize, weight: .black, design: .default))
                                 .padding()
                             Text(synonym).font(.system(size: K.Size.synonymTextSize, weight: .bold, design: .default))
@@ -59,90 +62,63 @@ struct ContentView: View {
                         )
                         .offset(x: 0, y: -150)
                         
-                    }.overlay(Text(idiomExp).font(.system(size: K.Size.expTextSize, weight: .medium, design: .default)).offset(x: 0, y: 100).frame(width: 380, height: 300))
-                }
-                //                if isCorrect {
-                //                    VStack {
-                //                        Group{
-                //                            // イディオムカード
-                //                            VStack() {
-                //                                Text(idiom).font(.system(size: K.Size.idiomTextSize, weight: .black, design: .default))
-                //                                Text(synonym).font(.system(size: K.Size.synonymTextSize, weight: .bold, design: .default))
-                //                            }.background(RoundedRectangle(cornerRadius: 50)
-                //                                .fill(Color(cardColor))
-                //                                .frame(width: 380, height: 300)
-                //                                .shadow(color:.gray, radius: 2, x:0, y:5)
-                //                            )
-                //                            .offset(x: 50, y: 50)
-                //
-                //                        }.transition(AnyTransition.scaleAndSlide).rotationEffect(.degrees(30))
-                //                    }.animation(.default, value:isCorrect)
-                //                }
-                
-                
-                // イディオムの説明
-                HStack(spacing:80){
-                    Button(action:{
-                        isIncorrect.toggle()
-                        viewModel.incorrectArray.append(generateIdioms())
-                    }) {
-                        Image("cross")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.all)
-                            .background(RoundedRectangle(cornerRadius: 25)
-                                .fill(Color(K.Colors.crossBtnColor))
-                                .frame(width: 100, height: 100)
-                                .shadow(color:.gray, radius: 2, x:0, y:5)
-                            )
-                            .frame(width: 100, height: 100)
                     }
-                    Button(action:{
-                        isCorrect.toggle()
-                        viewModel.memorizedArray.append(generateIdioms())
-                    }){
-                        Image("check-mark")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.all)
-                            .background(RoundedRectangle(cornerRadius: 25)
-                                .fill(Color(K.Colors.checkBtnColor))
+                    .overlay(Text(idiomExp).font(.system(size: K.Size.expTextSize, weight: .medium, design: .default)).offset(x: 0, y: 100).frame(width: 380, height: 300))
+//                }
+                
+//                Spacer()
+                
+                Group {
+                    // チェック・バツボタン
+                    HStack(spacing:80){
+                        Button(action:{
+                            isIncorrect.toggle()
+                            viewModel.incorrectArray.append(generateIdioms())
+                        }) {
+                            Image("cross")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(.all)
+                                .background(RoundedRectangle(cornerRadius: 25)
+                                    .fill(Color(K.Colors.crossBtnColor))
+                                    .frame(width: 100, height: 100)
+                                    .shadow(color:.gray, radius: 2, x:0, y:5)
+                                )
                                 .frame(width: 100, height: 100)
-                                .shadow(color:.gray, radius: 2, x:0, y:5)
-                            )
-                            .frame(width: 100, height: 100)
+                        }
+                        Button(action:{
+                            isCorrect.toggle()
+                            viewModel.memorizedArray.append(generateIdioms())
+                        }){
+                            Image("check-mark")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(.all)
+                                .background(RoundedRectangle(cornerRadius: 25)
+                                    .fill(Color(K.Colors.checkBtnColor))
+                                    .frame(width: 100, height: 100)
+                                    .shadow(color:.gray, radius: 2, x:0, y:5)
+                                )
+                                .frame(width: 100, height: 100)
+                            
+                        }
                     }
-                }.offset(x: 0, y: 200)
-                ZStack {
-//                    NavigationView {
-//
-//                        K.Colors.blueGradient
-//                        Text("Exit").fontWeight(.bold).foregroundColor(Color.white).padding()
-//                        NavigationLink(destination: ResultView()) {
-//                            Text("Do Something")
-//                        }
-//
-//                    }
                 }
-//                NavigationView {
-//                    ZStack {
-//                        K.Colors.blueGradient
-//                        Text("Exit").fontWeight(.bold).foregroundColor(Color.white).padding()
-//                        NavigationLink(destination: ResultView()) {Text("dhaskdh")}
-//                    }
-//                }.frame(width: 70, height: 50).cornerRadius(15).offset(x: 150, y: 300)
-                Button(action:{
+                // Exitボタン
+                .overlay(Button(action:{
                     isShowingView.toggle()
                 }){
                     ZStack{
                         K.Colors.blueGradient
                         Text("Exit").fontWeight(.bold).foregroundColor(Color.white).padding()
-
+                        
                     }.frame(width: 70, height: 50).cornerRadius(15)
                 }.background(RoundedRectangle(cornerRadius: 15)).offset(x: 150, y: 300).sheet(isPresented: $isShowingView) {
-                    ResultView()
-                }
+                    ResultView(viewModel: viewModel)
+                })
             }
+            
+            Spacer()
         }
     }
     
@@ -200,10 +176,10 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
 
 
--- Fake Distinction/ResultView.swift
+++ Fake Distinction/ResultView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct ResultView: View {
+    @ObservedObject var viewModel: ViewModel
+    private var incorrectArray: [Int]
+    private var correctArrray: [Int]
+    
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        self.incorrectArray = viewModel.incorrectArray
+        self.correctArrray = viewModel.memorizedArray
+    }
+
     var body: some View {
-        Text("Your result")
-        Text("You need to learn:")
-        Text("You memorized:")
+        Text("Your result").font(.system(size: K.Size.idiomTextSize, weight: .black, design: .default))
+        Text("You need to learn: \(incorrectArray.count) words")
+        Text("You memorized: \(correctArrray.count) words")
+//        let numsString = incorrectArray.map { String($0) }.joined(separator: ", ")
     }
 }
 
-struct ResultView_Previews: PreviewProvider {
-    static var previews: some View {
-        ResultView()
-    }
-}
+
+//struct ResultView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ResultView(viewModel: ViewModel)
+//    }
+//}
